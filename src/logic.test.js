78a49878e0420b637e896b1e1bfa804de7fa05e7
sansSpoon <sans.spoon@gmail.com>
2018-11-09 15:39:18 +1100

    add tJoin test

--- src/logic.test.js
+++ src/logic.test.js
@@ -5,3 +5,7 @@ import { fTen, tJoin, sumTho, period, cents, cleanAmount } from './logic';
 test('adds 100 + 10 + 1 to equal 111', () => {
   expect(sumTho(100, 10, 1)).toBe(111);
 });
+
+test('returns " and " when param1 is greater than 0 and param2 equals 0', () => {
+  expect(tJoin(1, 0)).toBe(' and ');
+});
