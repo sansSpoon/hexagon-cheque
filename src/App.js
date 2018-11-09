90a7ba45a135e155c0af410d0f10d234cc28c967
sansSpoon <sans.spoon@gmail.com>
2018-11-09 14:50:15 +1100

    export main logic functions and import into app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
 import React, { Component } from 'react';
+import { tJoin, sumTho, period, cents } from './logic';
 import logo from './logo.svg';
 import './App.css';
+
+// to be replaced with user input
+// will change num to immutable
+let num = ' $101511 ';
+// clean up the input
+num = num.trim().replace('$', '').replace(/,/g,'');
+num = parseFloat(num).toFixed(2).toString();
+num = num.split('.');
+
+const dlr = num[0].padStart(9,'0'); // ES17 padding, might need a polly
+const cnt = num[1];
+console.log(dlr);
+console.log(cnt);
+console.log(cnt.substring(0,1));
+console.log(cnt.substring(1,2));
+
+let arr = dlr.split("");
+
+// Don't really need to do this, could just access
+// the array by index, creating variables just helps
+// with legibility, also arr is now dead.
+const one = arr.pop(),
+      ten = arr.pop(),
+      hun = arr.pop(),
+      tho = arr.pop(),
+      tth = arr.pop(),
+      hth = arr.pop(),
+      mil = arr.pop(),
+      tml = arr.pop(),
+      hml = arr.pop();
+
+const mString = parseInt(mil + tml + hml) > 0 ? `${period(hml, tml, mil)} million` : '';
+const tString = parseInt(tho + tth + hth) > 0 ? `${period(hth, tth, tho)} thousand` : '';
+const oString = parseInt(one + ten + hun) > 0 ? `${period(hun, ten, one)}` : '';
+
+const dString = `${mString} ${tString.length>0?tString:''}${oString.length>0?tJoin(sumTho(mil, tml, hml), hth):''}${oString.length>0?tJoin(sumTho(tho, tth, hth), hun):''}${oString} dollars`;
+
+// This is a bit hacky, need to improve joining
+const dStringClean = dString.replace(/\s\s/g, ' ').trim();
+
+console.log(`${dStringClean}${cents(cnt.substring(0,1), cnt.substring(1,2))}`);
 
 class App extends Component {
   render() {
--- src/logic.js
+++ src/logic.js
@@ -34,37 +34,7 @@ const tens = {
 	9: 'ninety',
 };
 
-// to be replaced with user input
-// will change num to immutable
-let num = ' $101511 ';
-// clean up the input
-num = num.trim().replace('$', '').replace(/,/g,'');
-num = parseFloat(num).toFixed(2).toString();
-num = num.split('.');
-
-const dlr = num[0].padStart(9,'0'); // ES17 padding, might need a polly
-const cnt = num[1];
-console.log(dlr);
-console.log(cnt);
-console.log(cnt.substring(0,1));
-console.log(cnt.substring(1,2));
-
-let arr = dlr.split("");
-
-// Don't really need to do this, could just access
-// the array by index, creating variables just helps
-// with legibility, also arr is now dead.
-const one = arr.pop(),
-      ten = arr.pop(),
-      hun = arr.pop(),
-      tho = arr.pop(),
-      tth = arr.pop(),
-      hth = arr.pop(),
-      mil = arr.pop(),
-      tml = arr.pop(),
-      hml = arr.pop();
-
-function fTen(ten, one) {
+export function fTen(ten, one) {
 	let string = '';
 	if(parseInt(ten) === 0 && parseInt(one) > 0) {
 		string = `${ones[one]}`;
@@ -80,7 +50,7 @@ function fTen(ten, one) {
 	return string.trim();
 }
 
-function tJoin(x1000, x100)	{
+export function tJoin(x1000, x100)	{
 	if(parseInt(x1000) > 0 && parseInt(x100) === 0) {
 		return ' and ';
 	} else {
@@ -88,12 +58,12 @@ function tJoin(x1000, x100)	{
 	}
 }
 
-function sumTho(x100, x10, x1) {
+export function sumTho(x100, x10, x1) {
 	const sum = parseInt(x100) + parseInt(x10) + parseInt(x1);
 	return sum;
 }
 
-function period(x100, x10, x1) {
+export function period(x100, x10, x1) {
 	const hundred = parseInt(x100) > 0 ? `${ones[x100]} hundred`: '';
 	const ten = parseInt(x1 + x10) > 0 ? fTen(x10, x1) : '';
 	let and = ' and';
@@ -107,7 +77,7 @@ function period(x100, x10, x1) {
 	return string.trim();
 }
 
-function cents(x10, x1) {
+export function cents(x10, x1) {
 	let string = '';
 	if(parseInt(x10 + x1) === 1) {
 		string = ' and one cent'
@@ -118,14 +88,3 @@ function cents(x10, x1) {
 	}
 	return string;
 }
-
-const mString = parseInt(mil + tml + hml) > 0 ? `${period(hml, tml, mil)} million` : '';
-const tString = parseInt(tho + tth + hth) > 0 ? `${period(hth, tth, tho)} thousand` : '';
-const oString = parseInt(one + ten + hun) > 0 ? `${period(hun, ten, one)}` : '';
-
-const dString = `${mString} ${tString.length>0?tString:''}${oString.length>0?tJoin(sumTho(mil, tml, hml), hth):''}${oString.length>0?tJoin(sumTho(tho, tth, hth), hun):''}${oString} dollars`;
-
-// This is a bit hacky, need to improve joining
-const dStringClean = dString.replace(/\s\s/g, ' ').trim();
-
-console.log(`${dStringClean}${cents(cnt.substring(0,1), cnt.substring(1,2))}`);
